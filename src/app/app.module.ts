b744c8f16c3cd2a253bc853dd156c08c17479014
Daschy <damy.schaeken@e-motionlabs.co>
2019-10-15 11:09:02 +0200

    Wireframe+navbar
    Added a prototype wireframe and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,24 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header/header.component';
+import { HomeComponent } from './home/home.component';
+import { UpcomingMoviesComponent } from './upcoming-movies/upcoming-movies.component';
+import { MyMoviesComponent } from './my-movies/my-movies.component';
+import { MyProfileComponent } from './my-profile/my-profile.component';
+import { WatchlistComponent } from './my-movies/watchlist/watchlist.component';
+import { MyListComponent } from './my-movies/my-list/my-list.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    UpcomingMoviesComponent,
+    MyMoviesComponent,
+    MyProfileComponent,
+    WatchlistComponent,
+    MyListComponent
   ],
   imports: [
     BrowserModule,
