13895381c6ba9959242a0c253782bf0e7d6aadf7
Daschy <damy.schaeken@e-motionlabs.co>
2019-10-16 18:08:47 +0200

    General cleaning up

--- src/app/movie/movie.model.ts
+++ src/app/movie/movie.model.ts
@@ -3,14 +3,16 @@ export class Movie {
     public genre: string;
     public releaseDate: string | Date;
     public imagePath: string;
+    public description: string;
     public addedToWatchList: boolean;
 
-    constructor(name: string, genre: string, releaseDate: string|Date, imagePath: string, addedToWatchList: boolean)
+    constructor(name: string, genre: string, releaseDate: string|Date, imagePath: string, description: string, addedToWatchList: boolean)
     {
         this.name = name;
         this.genre = genre;
         this.releaseDate= releaseDate;
         this.imagePath= imagePath;
+        this.description= description;
         this.addedToWatchList= addedToWatchList;
     }
 }
