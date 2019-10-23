b987d14662703b033d6f7e22c3d43ed08921d00b
Daschy <damy.schaeken@e-motionlabs.co>
2019-10-23 18:19:15 +0200

    deleted pipe testing files

--- src/app/movie/pipes/mylist.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MylistPipe } from './mylist.pipe';
-
-describe('MylistPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MylistPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
