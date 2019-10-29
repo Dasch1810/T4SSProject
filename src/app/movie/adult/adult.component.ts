6807a4d1675f8f61208c8f566979fd6b515ae54d
Daschy <damy.schaeken@e-motionlabs.co>
2019-10-29 13:55:15 +0100

    Basic rework on adult page

--- src/app/movie/adult/adult.component.ts
+++ src/app/movie/adult/adult.component.ts
@@ -9,11 +9,16 @@ import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 export class AdultComponent implements OnInit {
   public safeURL: SafeResourceUrl;
   adult1: boolean= false;
-
+  tooYoung= false;
+  babyImgUrl= "https://storage.googleapis.com/www-24baby-cloud/2017/01/Baby-met-veel-haren.jpg"
+  question= true;
   constructor(private _sanitizer: DomSanitizer){
     this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/3UjuYtgMDIg');
  }
-
+ageCheck(){
+  this.tooYoung = true;
+  this.question = false;
+}
 validate1(){
   this.adult1=true;
 }
