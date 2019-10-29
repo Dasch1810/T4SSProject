6d55556ed660506f120ce2b4cd4e0d60bad4a934
Daschy <damy.schaeken@e-motionlabs.co>
2019-10-29 13:24:10 +0100

    Validation for 18+ content

--- src/app/movie/adult/adult.component.ts
+++ src/app/movie/adult/adult.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-adult',
@@ -6,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./adult.component.css']
 })
 export class AdultComponent implements OnInit {
+  public safeURL: SafeResourceUrl;
+  adult1: boolean= false;
 
-  constructor() { }
+  constructor(private _sanitizer: DomSanitizer){
+    this.safeURL = this._sanitizer.bypassSecurityTrustResourceUrl('https://www.youtube.com/embed/3UjuYtgMDIg');
+ }
 
+validate1(){
+  this.adult1=true;
+}
   ngOnInit() {
   }
 
